0fc776c2526ba1aebd920a74d36ea8573ac371e2
MaxKrsk83 <mcsimus.krsk@gmail.com>
2021-10-07 23:16:54 +0700

    добавил ИИ и переделал проверку победы

--- src/main/java/TicTacToy.java
+++ src/main/java/TicTacToy.java
@@ -1,45 +1,131 @@
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToy {
-    public static int SIZE = 13;
-    public static int DOTS_TO_WIN = 4;
+    public static int SIZE = 9;
+    public static int DOTS_TO_WIN = 5;
     public static char[][] map = new char[SIZE][SIZE];
     private static final char DOT_EMPTY = '.';
     private static final char DOT_HUM = 'X';
     private static final char DOT_AI = 'O';
+    private static Scanner sc = new Scanner(System.in);
+    private static Random rnd = new Random();
 
-    /**
-     *
-     * @param args
-     */
+
     public static void main(String[] args) {
-/*
-        int countMaskOfMap = countOfArray(map);
-        //System.out.println("Количество размещений маски на карте: " + countMaskOfMap*countMaskOfMap);
-        int [][] mask = new int[DOTS_TO_WIN][DOTS_TO_WIN];
-        initMask(mask);
-        fillMap(map,mask,countMaskOfMap);
-        //printArr(mask);
-        printArr(map);
-*/
-        //printArr(map);
         initMap();
-
-//        map[2][2] = DOT_HUM;
-//        map[3][3] = DOT_HUM;
-//        map[0][2] = DOT_HUM;
-//        map[0][0] = DOT_AI;
-//        map[2][5] = DOT_AI;
-//        map[4][4] = DOT_AI;
-        map[6][6] = DOT_AI;
-        map[5][0] = DOT_AI;
-        map[2][0] = DOT_AI;
-        map[4][0] = DOT_AI;
-        aiTurn();
-//        int f = findMove(8,8);
-        System.out.println("------------------");
-        printArr(map);
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if(isWin(DOT_HUM)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if(isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if(isWin(DOT_AI)) {
+                System.out.println("Победил ИИ");
+                break;
+            }
+            if(isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра окончена");
     }
+    private static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkWin(int x, int y, int cX, int cY, char symbol){
+        for (int i = 0; i < DOTS_TO_WIN; i++) {
+            int sumX = x + i * cX;
+            int sumY = y + i * cY;
+            if(sumX < SIZE & sumX >= 0 & sumY < SIZE & sumY >= 0){
+                if(map[sumX][sumY] != symbol){
+                    return false;
+                }
+            } else{
+                return false;
+            }
+        }
+        return true;
+    }
+    private static boolean isWin(char symbol) {
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                if (checkWin(x,y,1,0,symbol)){
+                    return true;
+                } else if (checkWin(x,y,0,1,symbol)){
+                    return true;
+                } else if (checkWin(x,y,-1,0,symbol)){
+                    return true;
+                } else if (checkWin(x,y,0,-1,symbol)){
+                    return true;
+                }else if (checkWin(x,y,1,1,symbol)){
+                    return true;
+                }else if (checkWin(x,y,-1,1,symbol)){
+                    return true;
+                }else if (checkWin(x,y,1,-1,symbol)){
+                    return true;
+                }else if (checkWin(x,y,-1,-1,symbol)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE)
+            return false;
+        if (map[x][y] == DOT_EMPTY)
+            return true;
+        return false;
+    }
+
+    private static void humanTurn() {
+        int x;
+        int y;
+
+        do {
+            System.out.println("Введите координаты в формате Х и У");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        map[x][y] = DOT_HUM;
+    }
+
+    private static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     private static void initMap() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -47,65 +133,130 @@ public class TicTacToy {
             }
         }
     }
-
-    public static void aiTurn(){
-        int [][] mask = fillMask();
-        printArr(mask);
+    public static int findMinCount(int [][] mask){
+        int min = DOTS_TO_WIN;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (mask[i][j] < min & mask[i][j] != 0){
+                    min = mask[i][j];
+                }
+            }
+        }
+        return min;
     }
-    public static int[][] fillMask(){
+    public static ArrayList findMoves(int [][] mask, int index){
+        ArrayList result = new ArrayList();
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if(mask[i][j]==index){
+                    Point pt = new Point();
+                    pt.x = i;
+                    pt.y = j;
+                    result.add(pt);
+                }
+            }
+        }
+        return result;
+    }
+    public static Point getPoint(ArrayList arr){
+        return (Point)arr.get(rnd.nextInt(arr.size()));
+    }
+    public static void aiTurn(){
+        Point pt;
+        int [][] wins = fillMask(DOT_HUM);
+        int [][] loose = fillMask(DOT_AI);
+        int minCountToWins = findMinCount(wins);
+        int minCountToLoose = findMinCount(loose);
+        ArrayList moveWins = findMoves(wins,minCountToWins);
+        ArrayList moveLoose = findMoves(loose,minCountToLoose);
+        ArrayList commonMoves = findCommonMoves(moveWins,moveLoose);
+        ArrayList combo = findMoves(wins,2);
+        if (combo.size()>0){
+            pt = getPoint(combo);
+        }else {
+            if(minCountToWins > minCountToLoose) {
+                int sz = commonMoves.size();
+                if (sz > 0 ){
+                    pt = getPoint(commonMoves);
+                } else {
+                    pt = getPoint(moveLoose);
+                }
+            } else {
+                int sz = commonMoves.size();
+                if (sz > 0 ){
+                    pt = getPoint(commonMoves);
+                } else {
+                    pt = getPoint(moveWins);
+                }
+            }
+        }
+
+        map[pt.x][pt.y] = DOT_AI;
+    }
+    public static ArrayList findCommonMoves(ArrayList arr1, ArrayList arr2){
+        ArrayList result = new ArrayList();
+        for (int i = 0; i < arr1.size(); i++) {
+            for (int j = 0; j < arr2.size(); j++) {
+                if (arr1.get(i)==arr2.get(j)){
+                    result.add(arr1.get(i));
+                }
+            }
+        }
+        return result;
+    }
+    public static int[][] fillMask(char whoWalks){
         int [][] mask = new int[SIZE][SIZE];
         for (int x = 0; x < SIZE; x++) {
             for (int y = 0; y < SIZE; y++) {
                 if(map[x][y]!=DOT_EMPTY){
                     mask[x][y]=0;
                 }else{
-                    mask[x][y] = check(x,y);
+                    mask[x][y] = check(x,y,whoWalks);
                 }
             }
         }
         return mask;
     }
-    public static int check(int x, int y){
-        int count = DOTS_TO_WIN;
-        int tmp = checkDir(x,y,1,0);
-        if (tmp<count & tmp>0){
-            count = tmp;
-        }
-        tmp = checkDir(x,y,0,1);
-        if (tmp<count & tmp>0){
-            count = tmp;
-        }
-        tmp = checkDir(x,y,-1,0);
-        if (tmp<count & tmp>0){
-            count = tmp;
-        }
-        tmp = checkDir(x,y,0,-1);
-        if (tmp<count & tmp>0){
-            count = tmp;
-        }
-        tmp = checkDir(x,y,1,1);
-        if (tmp<count & tmp>0){
-            count = tmp;
-        }
-        tmp = checkDir(x,y,-1,1);
-        if (tmp<count & tmp>0){
-            count = tmp;
-        }
-        tmp = checkDir(x,y,-1,-1);
-        if (tmp<count & tmp>0){
-            count = tmp;
-        }
-        tmp = checkDir(x,y,1,-1);
-        if (tmp<count & tmp>0){
-            count = tmp;
-        }
+    public static int check(int x, int y, char whoWalks){
 
+        int count = DOTS_TO_WIN;
+        int tmp = checkDir(x,y,1,0, whoWalks);
+        if (tmp<count & tmp>0){
+            count = tmp;
+        }
+        tmp = checkDir(x,y,0,1, whoWalks);
+        if (tmp<count & tmp>0){
+            count = tmp;
+        }
+        tmp = checkDir(x,y,-1,0, whoWalks);
+        if (tmp<count & tmp>0){
+            count = tmp;
+        }
+        tmp = checkDir(x,y,0,-1, whoWalks);
+        if (tmp<count & tmp>0){
+            count = tmp;
+        }
+        tmp = checkDir(x,y,1,1, whoWalks);
+        if (tmp<count & tmp>0){
+            count = tmp;
+        }
+        tmp = checkDir(x,y,-1,1, whoWalks);
+        if (tmp<count & tmp>0){
+            count = tmp;
+        }
+        tmp = checkDir(x,y,-1,-1, whoWalks);
+        if (tmp<count & tmp>0){
+            count = tmp;
+        }
+        tmp = checkDir(x,y,1,-1, whoWalks);
+        if (tmp<count & tmp>0){
+            count = tmp;
+        }
         return count;
     }
-    public static int checkDir(int x, int y, int cX, int cY){
+    public static int check1(int x, int y, int cX, int cY, char whoWalks){
         int count = 0;
-//проверка
-        for (int i = -DOTS_TO_WIN + 1; i < DOTS_TO_WIN; i++) {
+        for (int i = 0; i < DOTS_TO_WIN; i++) {
             int sumX = x + i * cX;
             int sumY = y + i * cY;
             if (!(sumX < SIZE & sumX >=0) || !(sumY < SIZE & sumY >=0) ){
@@ -113,104 +264,22 @@ public class TicTacToy {
             }
             if (map[sumX][sumY] == DOT_EMPTY){
                     count++;
-            } if (map[sumX][sumY] == DOT_HUM){
+            } if (map[sumX][sumY] == whoWalks){
                 return 0;
             }
         }
         return count;
     }
-    public static int checkD1(int x, int y){
-        int count = 0;
-        for (int i = 0; i < DOTS_TO_WIN; i++) {
-            int sumX = x + i;
-            int sumY = y + i;
-            if (sumX < SIZE & sumX >=0 & sumY < SIZE & sumY >=0){
-                if (map[sumX][sumY] == DOT_EMPTY){
-                    count++;
-                } if (map[sumX][sumY] == DOT_HUM){
-                    count = 0;
-                    break;
-                }
-            } else{
-                count = 0;
-                break;
+    public static int checkDir(int x, int y, int cX, int cY, char whoWalks){
+        int min = DOTS_TO_WIN;
+        for (int i = -DOTS_TO_WIN+1 ; i <= 0; i++) {
+            int tmp = check1(x + i * cX,y + i * cY, cX, cY, whoWalks);
+            if(tmp < min & tmp > 0){
+                min = tmp;
             }
         }
-        return count;
+        return min;
     }
-    public static int checkD2(int x, int y){
-        int count = 0;
-        for (int i = 0; i < DOTS_TO_WIN; i++) {
-            int sumX = x - i;
-            int sumY = y + i;
-            if (sumX < SIZE & sumX >=0 & sumY < SIZE & sumY >=0){
-                if (map[sumX][sumY] == DOT_EMPTY){
-                    count++;
-                } if (map[sumX][sumY] == DOT_HUM){
-                    count = 0;
-                    break;
-                }
-            } else{
-                count = 0;
-                break;
-            }
-        }
-        return count;
-    }
-
-
-    public static void fillMap(int [][] map,int [][] mask, int count){
-        for (int i = 0; i < count; i++) {
-            for (int j = 0; j < count; j++) {
-                fillMapByMask(map,mask,i,j);
-            }
-        }
-    }
-    public static void fillMapByMask(int [][] map,int [][] mask, int x, int y){
-        for (int i = 0; i < mask.length; i++) {
-            for (int j = 0; j < mask[i].length; j++) {
-                if (map[i+x][j+y]==0){
-                    map[i+x][j+y] = map[i+x][j+y] + mask[i][j];
-                } else{
-                    map[i+x][j+y] = map[i+x][j+y] + mask[i][j] - 1;
-                }
-            }
-        }
-    }
-    public static int countOfArray(int arr[][]){
-        return arr.length - DOTS_TO_WIN + 1;
-    }
-    public static void fillMaskH(int arr[][]){
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = arr[i][j] + 1;
-            }
-        }
-    }
-    public static void fillMaskV(int arr[][]){
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[j][i] = arr[j][i] + 1;
-            }
-        }
-    }
-    public static void fillMaskD(int arr[][]){
-        for (int i = 0; i < arr.length; i++) {
-            arr[i][i] = arr[i][i] + 1;
-        }
-    }
-    public static void fillMaskD1(int arr[][]){
-        for (int i = 0; i < arr.length; i++) {
-            arr[arr.length - i -1][i] = arr[arr.length - i - 1][i] + 1;
-        }
-    }
-    public static void initMask(int arr[][]){
-        fillMaskH(arr);
-        fillMaskV(arr);
-        fillMaskD(arr);
-        fillMaskD1(arr);
-    }
-
     public static void printArr(int arr[][]){
         for (int i = 0; i < arr.length; i++) {
             System.out.println(Arrays.toString(arr[i]));
