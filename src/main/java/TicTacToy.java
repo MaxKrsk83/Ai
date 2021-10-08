423652aa362137bf65c39cc891a677c7c55d14f9
MaxKrsk83 <mcsimus.krsk@gmail.com>
2021-10-08 11:57:41 +0700

    Доработал ии

--- src/main/java/TicTacToy.java
+++ src/main/java/TicTacToy.java
@@ -317,14 +317,4 @@ public class TicTacToy {
         }
         return min;
     }
-    public static void printArr(int arr[][]){
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(Arrays.toString(arr[i]));
-        }
-    }
-    public static void printArr(char arr[][]){
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(Arrays.toString(arr[i]));
-        }
-    }
 }
